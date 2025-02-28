252e26044c782058a84921623858abfb7aeec69d
cfifeg1@gmail.com <cfifeg1@gmail.com>
2025-02-28 13:12:38 +0300

    try recaptcha

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
 import { SmartCaptcha } from '@yandex/smart-captcha';
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
+import ReCAPTCHA from "react-google-recaptcha";
 
 function App() {
-  const [token, setToken] = useState<string>()
+    const [token, setToken] = useState<string>()
+    const [captchaVariant, setCaptchaVariant] = useState(1)
 
-  return ( 
-    <div>
-      {token && <><p>token: <span style={{ color: "red"}}>{token}</span></p><br /></>}
-      
-      <SmartCaptcha 
-        sitekey="ysc1_PLzNIMA5rFE9dXkEXdHAsbaPb20qFaGZ9GQsk17K5ae9fb61" 
-        onJavascriptError={(e) => console.log(`JS Error:`, e)} 
-        onNetworkError={() => console.info(`NETWORK ERROR`)}
-        onSuccess={setToken} 
-      />
-    </div>
-  )
+    useEffect(() => {
+        setToken(undefined)
+    }, [captchaVariant]);
+
+    return (
+        <div>
+
+            <div style={{display: "flex", gap: '16px'}}>
+                <button onClick={setCaptchaVariant.bind(null, 1)}>YandexCaptcha</button>
+                <button onClick={setCaptchaVariant.bind(null, 2)}>ReCAPTCHA</button>
+            </div>
+            <br />
+            {captchaVariant === 1 && <SmartCaptcha
+                sitekey="ysc1_PLzNIMA5rFE9dXkEXdHAsbaPb20qFaGZ9GQsk17K5ae9fb61"
+                onJavascriptError={(e) => console.log(`JS Error:`, e)}
+                onNetworkError={() => console.info(`NETWORK ERROR`)}
+                onSuccess={setToken}
+            />
+            }
+            {captchaVariant === 2 &&
+                <ReCAPTCHA onChange={token => setToken(token as string)} sitekey={'6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'}/>
+            }
+
+            <div>
+                {token && <><p>token: <span style={{ color: "red"}}>{token}</span></p><br /></>}
+            </div>
+        </div>
+    )
 }
 
 export default App
