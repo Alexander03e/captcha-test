cb80315dc482d6ce5bd2338f5656af2851dc6e28
cfifeg1@gmail.com <cfifeg1@gmail.com>
2025-02-28 11:22:51 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { SmartCaptcha } from '@yandex/smart-captcha';
+import { useState } from 'react'
+
+function App() {
+  const [token, setToken] = useState<string>()
+
+  return ( 
+    <div>
+      {token && <><p>token: <span style={{whiteSpace: "wrap", color: "red"}}>{token}</span></p><br /></>}
+      
+      <SmartCaptcha 
+        sitekey="ysc1_PLzNIMA5rFE9dXkEXdHAsbaPb20qFaGZ9GQsk17K5ae9fb61" 
+        onJavascriptError={(e) => console.log(`JS Error: ${e}`)} 
+        onNetworkError={() => console.info(`NETWORK ERROR`)}
+        onSuccess={setToken} 
+      />
+    </div>
+  )
+}
+
+export default App
