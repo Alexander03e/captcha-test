27177da30fdd2df186341c99c6a2044a91a60ce2
cfifeg1@gmail.com <cfifeg1@gmail.com>
2025-02-28 13:19:56 +0300

    er

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,45 @@ import ReCAPTCHA from "react-google-recaptcha";
 
 function App() {
     const [token, setToken] = useState<string>()
+    const [error, setError] = useState()
     const [captchaVariant, setCaptchaVariant] = useState(1)
+
 
     useEffect(() => {
         setToken(undefined)
+        setError(undefined)
     }, [captchaVariant]);
+
+    const activeStyle = {
+        background: "red"
+    }
 
     return (
         <div>
 
             <div style={{display: "flex", gap: '16px'}}>
-                <button onClick={setCaptchaVariant.bind(null, 1)}>YandexCaptcha</button>
-                <button onClick={setCaptchaVariant.bind(null, 2)}>ReCAPTCHA</button>
+                <button style={captchaVariant === 1 ? activeStyle : {}} onClick={setCaptchaVariant.bind(null, 1)}>YandexCaptcha</button>
+                <button style={captchaVariant === 2 ? activeStyle : {}} onClick={setCaptchaVariant.bind(null, 2)}>ReCAPTCHA</button>
             </div>
+
             <br />
-            {captchaVariant === 1 && <SmartCaptcha
-                sitekey="ysc1_PLzNIMA5rFE9dXkEXdHAsbaPb20qFaGZ9GQsk17K5ae9fb61"
-                onJavascriptError={(e) => console.log(`JS Error:`, e)}
-                onNetworkError={() => console.info(`NETWORK ERROR`)}
-                onSuccess={setToken}
-            />
+            {captchaVariant === 1 &&
+                <SmartCaptcha
+                    sitekey="ysc1_PLzNIMA5rFE9dXkEXdHAsbaPb20qFaGZ9GQsk17K5ae9fb61"
+                    onJavascriptError={(e) => {
+                        console.log(`JS Error:`, e)
+                        setError(e)
+                    }}
+                    onNetworkError={() => console.info(`NETWORK ERROR`)}
+                    onSuccess={setToken}
+                />
             }
             {captchaVariant === 2 &&
                 <ReCAPTCHA onChange={token => setToken(token as string)} sitekey={'6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'}/>
             }
 
             <div>
+                {error && <p style={{color: "red"}}>ERROR: {JSON.stringify(error)}</p>}
                 {token && <><p>token: <span style={{ color: "red"}}>{token}</span></p><br /></>}
             </div>
         </div>
